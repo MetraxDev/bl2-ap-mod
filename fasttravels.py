b98f3b99abfa1b2238bb18dd277073e921a795c9
Marco <m.spreckels@hotmail.de>
2025-10-11 18:29:13 +0200

    cleanup fast travels, method for checks, only base game tps

--- fasttravels.py
+++ fasttravels.py
@@ -1,3 +1,4 @@
+import os
 from mods_base import (
     command, 
     get_pc,
@@ -18,6 +19,22 @@ def get_fasttravel_definitions():
             logging.info(f"Fast Travel: {fasttravel}")
             logging.info(f"Fast Travel: {fasttravel.StationDisplayName}")
             logging.info(f"Fast Travel: {fasttravel.MissionDependencies}")
+            logging.info(f"Fast Travel: {fasttravel.bSendOnly}")
+            logging.info(f"Fast Travel: {fasttravel.bInitiallyActive}")
+            logging.info(f"Fast Travel: {fasttravel.DlcExpansion}")
+
+def export_fasttravel_names():
+    import json
+    if get_pc():
+        fasttravels = get_pc().GetWillowGlobals().GetFastTravelStationsLookup().FastTravelStationLookupList
+        names = []
+        for fasttravel in fasttravels:
+            if fasttravel.bSendOnly or fasttravel.DlcExpansion: continue
+
+            names.append(fasttravel.StationDisplayName)
+
+        with open(os.path.join(os.path.expandvars(r"%localappdata%/BL2Archipelago"), "ap_fasttravels.json"), "w") as f:
+            json.dump(names, f, indent=2)
 
 def get_fasttravel_definition_by_name(name):
     if get_pc():
@@ -32,30 +49,26 @@ def try_teleport_to_fasttravel_station(name):
             if name in fasttravel.StationDisplayName:
                 get_pc().ServerTeleportPlayerToStation(fasttravel)
 
-def get_fasttravels():
-    if get_pc():
-        for station in find_all("FastTravelStation"):
-            logging.info(f"Fast Travel: {station}")
-
-def set_fasttravels_usability():
-    if get_pc():
-        for station in find_all("FastTravelStation"):
-            if station and station.InteractiveObjectDefinition:
-                station.SetUsability(True, 1)
-
-def register_fasttravels():
-    if get_pc():
-        for fasttravel in get_pc().GetWillowGlobals().GetFastTravelStationsLookup().FastTravelStationLookupList:
-            fasttravel.MissionDependencies = []
-            get_pc().Behavior_RegisterStationDefinition(fasttravel, True)
-
 def register_fasttravel(name):
     if get_pc():
         for fasttravel in get_pc().GetWillowGlobals().GetFastTravelStationsLookup().FastTravelStationLookupList:
+            if fasttravel.bSendOnly or fasttravel.DlcExpansion: continue
+
             if name == fasttravel.StationDisplayName:
+                fasttravel.MissionDependencies = []
                 locationDisplayNames.append(fasttravel.StationDisplayName)
                 locationStationDefinitions.append(fasttravel)
                 locationStationStrings.append(fasttravel.StationDisplayName)
+
+def register_all_fasttravel():
+    if get_pc():
+        for fasttravel in get_pc().GetWillowGlobals().GetFastTravelStationsLookup().FastTravelStationLookupList:
+            if fasttravel.bSendOnly or fasttravel.DlcExpansion: continue
+            
+            fasttravel.MissionDependencies = []
+            locationDisplayNames.append(fasttravel.StationDisplayName)
+            locationStationDefinitions.append(fasttravel)
+            locationStationStrings.append(fasttravel.StationDisplayName)
 
 def unregister_fasttravel(name):
     if get_pc():
@@ -65,21 +78,9 @@ def unregister_fasttravel(name):
                 locationStationDefinitions.remove(fasttravel)
                 locationStationStrings.remove(fasttravel.StationDisplayName)
 
-@command("ap_get_fasttravels", description="Get all fast travel locations and their unlock status")
-def cmd_get_fasttravels(args):
-    get_fasttravels()
-
-@command("ap_register_fasttravels", description="Register all fast travel locations")
-def cmd_register_fasttravels(args):
-    register_fasttravels()
-
 @command("ap_get_fasttravel_defs", description="Get all fast travel definitions")
 def cmd_get_fasttravel_defs(args):
     get_fasttravel_definitions()
-
-@command("ap_set_fasttravels_usable", description="Set all fast travel locations to usable")
-def cmd_set_fasttravels_usable(args):
-    set_fasttravels_usability()
 
 @command("ap_teleport_to_sanctuary", description="Teleport to Sanctuary fast travel station")
 def cmd_teleport_to_sanctuary(args):
@@ -91,104 +92,45 @@ def cmd_register_fasttravel(args):
     register_fasttravel(args.name)
 cmd_register_fasttravel.add_argument("name", type=str, help="Name of the fast travel station to register")
 
+@command("ap_register_all_fasttravels", description="Register all fast travel locations")
+def cmd_register_all_fasttravels(args):
+    register_all_fasttravel()
+
 @command("ap_unregister_fasttravel", description="Unregister a specific fast travel location")
 def cmd_unregister_fasttravel(args):
     unregister_fasttravel(args.name)
 cmd_unregister_fasttravel.add_argument("name", type=str, help="Name of the fast travel station to unregister")
 
+@command("ap_export_fasttravel_names", description="Export all fast travel names to a JSON file")
+def cmd_export_fasttravel_names(args):
+    export_fasttravel_names()
+
 commands = [
-    cmd_get_fasttravels,
-    cmd_register_fasttravels,
     cmd_get_fasttravel_defs,
-    cmd_set_fasttravels_usable,
     cmd_teleport_to_sanctuary,
     cmd_register_fasttravel,
     cmd_unregister_fasttravel,
+    cmd_register_all_fasttravels,
+    cmd_export_fasttravel_names
 ]
-
-@hook("WillowGame.FastTravelStationGFxMovie:HandleOpen", Type.PRE)
-def HandleOpen(obj: UObject, args: WrappedStruct, ret: Any, func: BoundFunction) -> Any:
-    logging.info(f"[Archipelago] Fast Travel Station Opened: {obj}")
-    # obj.LocationDisplayNames = []
-    # obj.LocationStationDefinitions = []
-    # obj.LocationStationStrings = []
-    return False
-
-@hook("WillowGame.FastTravelStationGFxMovie:HandleOpen", Type.POST)
-def HandleOpenPost(obj: UObject, args: WrappedStruct, ret: Any, func: BoundFunction) -> Any:
-    logging.info(f"[Archipelago] POST Fast Travel Station Opened: {obj}")
-    # obj.LocationDisplayNames = []
-    # obj.LocationStationDefinitions = []
-    # obj.LocationStationStrings = []
-    return False
-
-@hook("WillowGame.FastTravelStationGFxMovie:BuildLocationData", Type.PRE)
-def BuildLocationData(obj: UObject, args: WrappedStruct, ret: Any, func: BoundFunction) -> Any:
-    logging.info(f"[Archipelago] Building Location Data for: {obj}")
-    return False
 
 @hook("WillowGame.FastTravelStationGFxMovie:BuildLocationData", Type.POST)
 def BuildLocationDataPost(obj: UObject, args: WrappedStruct, ret: Any, func: BoundFunction) -> Any:
-    logging.info(f"[Archipelago] POST Building Location Data for: {obj}")
-
-    # for fasttravel in get_pc().GetWillowGlobals().GetFastTravelStationsLookup().FastTravelStationLookupList:
-    #     obj.LocationDisplayNames.append(fasttravel.StationDisplayName)
-    #     obj.LocationStationDefinitions.append(fasttravel)
-    #     obj.LocationStationStrings.append(fasttravel.StationDisplayName)
-
     obj.LocationDisplayNames = locationDisplayNames
+    obj.LocationDisplayNamesAlphabetical = sorted(locationDisplayNames)
     obj.LocationStationDefinitions = locationStationDefinitions
     obj.LocationStationStrings = locationStationStrings
+    obj.LocationIsHeader = [False] * len(locationDisplayNames)
 
     return False
 
-@hook("WillowGame.FastTravelStationGFxMovie:extActivate", Type.PRE)
-def ExtActivate(obj: UObject, args: WrappedStruct, ret: Any, func: BoundFunction) -> Any:
-    logging.info(f"[Archipelago] ExtActivate called for: {obj}")
+@hook("WillowGame.PlayerBehavior_RegisterFastTravelStation:ApplyBehaviorToContext")
+def RegisterFastTravelStation(obj: UObject, args: WrappedStruct, ret: Any, func: BoundFunction) -> Any:
+    logging.info(f"[Archipelago] Found Station: {args.SelfObject.GetTravelStationDefinition().StationDisplayName}")
+
     return False
-
-@hook("WillowGame.FastTravelStationGFxMovie:extActivate", Type.POST)
-def ExtActivatePost(obj: UObject, args: WrappedStruct, ret: Any, func: BoundFunction) -> Any:
-    logging.info(f"[Archipelago] POST ExtActivate called for: {obj}")
-    return False
-
-@hook("WillowGame.FastTravelStationGFxObject:SendLocationData", Type.PRE)
-def SendLocationData(obj: UObject, args: WrappedStruct, ret: Any, func: BoundFunction) -> Any:
-    logging.info(f"[Archipelago] Sending Location Data for: {obj}")
-    # for displayName in args.LocationDisplayNames:
-    #     logging.info(f"[Archipelago] Arg: {displayName}")
-    #     displayName = "Fast Travel Disabled"
-    # for stationStrings in args.LocationStationNames:
-    #     logging.info(f"[Archipelago] Arg: {stationStrings}")
-    #     stationStrings = "Fast Travel Disabled"
-
-    # Infinite recursion, oops
-    #func(args.LocationDisplayNames, args.LocationStationNames, args.InitialSelectionIndex, args.CurrentWaypointIndex)
-    return False
-
-@hook("WillowGame.FastTravelStationGFxObject:SendLocationData", Type.POST)
-def SendLocationDataPost(obj: UObject, args: WrappedStruct, ret: Any, func: BoundFunction) -> Any:
-    logging.info(f"[Archipelago] POST Sending Location Data for: {obj}")
-    # for displayName in args.LocationDisplayNames:
-    #     logging.info(f"[Archipelago] Arg: {displayName}")
-    #     displayName = "Fast Travel Disabled"
-    # for stationStrings in args.LocationStationNames:
-    #     logging.info(f"[Archipelago] Arg: {stationStrings}")
-    #     stationStrings = "Fast Travel Disabled"
-
-    # Infinite recursion, oops
-    #func(args.LocationDisplayNames, args.LocationStationNames, args.InitialSelectionIndex, args.CurrentWaypointIndex)
-    return False
-
-#/mnt/SSD/Projects/Archipelago/bl2-mods/downloaded/loot/Mod/tps/locations.py
 
 hooks = [
-    HandleOpen,
-    HandleOpenPost,
-    BuildLocationData,
-    SendLocationData,
-    ExtActivate,
     BuildLocationDataPost,
-    SendLocationDataPost,
-    ExtActivatePost,
+    RegisterFastTravelStation,
 ]
